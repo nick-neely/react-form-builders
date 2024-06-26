6da5d8171b77dcfc8595f65080c884e0f0d08409
nick-neely <neelynickolas@gmail.com>
2024-06-26 09:40:38 -0500

    Refactor FormEngine to filter out allowed components

--- src/components/FormEngine.tsx
+++ src/components/FormEngine.tsx
@@ -1,8 +1,20 @@
 import { rSuiteComponents } from "@react-form-builder/components-rsuite";
 import { BuilderView, FormBuilder } from "@react-form-builder/designer";
 
+const allowedComponents = [
+  "RsInput",
+  "RsSignature",
+  "RsTextArea",
+  "RsHeader",
+  "RsImage",
+  "RsLink",
+];
+
 const FormEngine = () => {
-  const components = rSuiteComponents.map((c) => c.build());
+  const components = rSuiteComponents
+    .map((c) => c.build())
+    .filter((component) => allowedComponents.includes(component.meta.type));
+
   const builderView = new BuilderView(components);
   return <FormBuilder view={builderView} />;
 };
